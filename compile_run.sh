7c7482c1fbb94a177c71c68bee15c4185b09c215
ka <kaku.h9@gmail.com>
2026-01-27 22:13:34 +0000

    updated compile_run.sh

--- compile_run.sh
+++ compile_run.sh
@@ -1,12 +1,16 @@
 
 CXXSRC="
   src/graphics/loaders/qoi_loader.cpp
+  src/graphics/framebuffer.cpp
+  src/graphics/renderer.cpp
+  src/graphics/window.cpp
 "
 
 rm ./main
 
 
 g++ src/main.cpp -std=c++23 -g -o main  \
+     -Isrc \
      ${CXXSRC} \
      -lsfml-graphics -lsfml-window -lsfml-system \
     
