9b44bec5b62707a30fc33fad21497951a92e5586
ka <kaku.h9@gmail.com>
2026-01-27 22:12:53 +0000

    graphics: added window type

---
+++ src/graphics/window.cpp
@@ -0,0 +1,49 @@
+#include "window.hpp"
+#include "graphics/framebuffer.hpp"
+
+namespace KissShock{
+  Window::Window()
+    :
+      m_window{sf::VideoMode(HEIGHT, WIDTH), "KissShock Engine", sf::Style::Resize},
+      m_view{sf::FloatRect{0,  0, 320, 200}}
+  {
+    m_window.setView(m_view);
+    m_fbTexture.create(320, 200);
+  }
+
+  void Window::HandleEvents(){
+    sf::Event event;
+    while(m_window.pollEvent(event)){
+      if (event.type == sf::Event::Closed){
+        std::println("Exiting...");
+        m_window.close();
+      }
+    }
+  }
+
+  void Window::SetFrameBuffer(FrameBuffer& fb){
+    m_frameBuffer = &fb;
+  }
+
+  void Window::SwapFrameBuffer(FrameBuffer* fb){
+    FrameBuffer* tmp = m_frameBuffer;
+    m_frameBuffer = fb;
+    fb = tmp;
+  }
+
+  void Window::UpdateTexture(){
+    if(!m_frameBuffer){
+      return;
+    }
+    m_fbTexture.update(m_frameBuffer->Get());
+    m_fbSprite.setTexture(m_fbTexture);
+  }
+
+  void Window::Draw(){
+    m_window.clear();
+    UpdateTexture();
+    m_window.draw(m_fbSprite);
+    m_window.display();
+  }
+
+}
--- src/graphics/window.hpp
+++ src/graphics/window.hpp
@@ -1,5 +1,40 @@
-namespace KissShock{
-  class Window{
+#pragma once
 
+#include "SFML/Graphics/RenderWindow.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/WindowStyle.hpp>
+#include <print>
+
+// uses sfml as the window api, maybe later i'll switch out to sdl if it turns out sfml is not multiplatform
+//
+// window to a 320x200 screen for the og experience
+
+namespace KissShock{
+  class FrameBuffer;
+
+  class Window{
+    static constexpr std::size_t HEIGHT = 320;
+    static constexpr std::size_t WIDTH = 200;
+
+    public:
+      Window();
+
+      void HandleEvents();
+
+      void SetFrameBuffer(FrameBuffer& fb);
+      void SwapFrameBuffer(FrameBuffer* fb);
+
+      void UpdateTexture();
+
+      void Draw();
+
+      bool IsOpen(){return m_window.isOpen();}
+
+    private:
+      sf::RenderWindow m_window;
+      sf::View m_view;
+      sf::Texture m_fbTexture;
+      sf::Sprite m_fbSprite;
+      FrameBuffer* m_frameBuffer;
   };
 }
