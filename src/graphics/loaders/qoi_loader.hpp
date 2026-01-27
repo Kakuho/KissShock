f2927fbc380c6ed4fb9afa9c1d7114dde23fb8f0
ka <kaku.h9@gmail.com>
2026-01-27 14:50:29 +0000

    graphics: added qoi header struct

--- src/graphics/loaders/qoi_loader.hpp
+++ src/graphics/loaders/qoi_loader.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
-// for now we support QOI but later i will add more file formats
+//  for now we support QOI but later i will add more file formats
 //
-// https://en.wikipedia.org/wiki/QOI_(image_format)
+//  https://en.wikipedia.org/wiki/QOI_(image_format)
+//
+//  Deocode() will return a std::vector<std::uint8_t> containing the decoded image pixels
 
 #include <array>
 #include <cstdint>
@@ -13,10 +15,20 @@
 
 namespace KissShock{
   class QoiLoader{
+
+    struct QoiHeader{
+      std::uint8_t header[4];
+      std::uint32_t width;
+      std::uint32_t height;
+      std::uint8_t channels;
+      std::uint8_t colourspace;
+    };
+
     public:
       QoiLoader(std::string_view filename);
       
       void PrintBuffer() const;
+      std::vector<std::uint8_t> Decode() const;
 
     private:
       void IsValid() const;
