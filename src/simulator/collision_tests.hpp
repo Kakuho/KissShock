9e83f879569fc4f692f7124cd6f7cfc72f757a9e
ka <kaku.h9@gmail.com>
2026-02-03 14:34:51 +0000

    simulator: removed collision_handler.hpp and made tests private static members of the table

--- src/simulator/collision_tests.hpp
+++ src/simulator/collision_tests.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+// Type CollisionTestTable is a class which represents an interface to the collision tests.
+// It is this type which should be queried using the multiple subscript operator in order
+// to use collision detection tests
+
 #include <cstdint>
+#include <array>
+#include <functional>
 
 #include "collision.hpp"
 
@@ -8,21 +14,26 @@ namespace KissShock{
 
   class CollisionTestTable{
     using TestHandler = bool(Collision&, Collision&);
+    using TestWrapper = std::function<TestHandler>;
     public:
       constexpr TestHandler& operator[](Collision& coll1, Collision& coll2) const;
 
     private:
+      // to simplify collision test querying
       TestHandler& LhsSquare(Collision& lhs, Collision& rhs) const;
       TestHandler& LhsCircle(Collision& lhs, Collision& rhs) const;
       TestHandler& LhsEllipse(Collision& lhs, Collision& rhs) const;
-  };
 
-  bool HandleSquareVsSquare(Collision& square1, Collision& square2);
-  bool HandleSquareVsCircle(Collision& square, Collision& circle);
-  bool HandleSquareVsEllipse(Collision& square, Collision& ellipse);
-  bool HandleCircleVsCircle(Collision& circle1, Collision& circle2);
-  bool HandleCircleVsEllipse(Collision& circle, Collision& ellipse);
-  bool HandleEllipseVsEllipse(Collision& ellipse1, Collision& ellipse2);
+      // the collision tests themselves
+      static bool TestSquareVsSquare(Collision& square1, Collision& square2);
+      static bool TestSquareVsCircle(Collision& square, Collision& circle);
+      static bool TestSquareVsEllipse(Collision& square, Collision& ellipse);
+      static bool TestCircleVsCircle(Collision& circle1, Collision& circle2);
+      static bool TestCircleVsEllipse(Collision& circle, Collision& ellipse);
+      static bool TestEllipseVsEllipse(Collision& ellipse1, Collision& ellipse2);
+
+      std::array<TestWrapper, 9> m_handlers;
+  };
 
   constexpr auto CollisionTestTable::operator[](Collision& col1, Collision& col2) const -> TestHandler&{
     using enum Collision::Type;
