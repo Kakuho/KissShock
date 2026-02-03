3d10416748aa002209de863f056f2d0bc5a38729
ka <kaku.h9@gmail.com>
2026-02-03 14:37:07 +0000

    removed physics directory, renamed to simulator

--- src/physics/pengine2d.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Physics Engine for 2d games
-
-namespace KissShock{
-  class PEngine2D{
-
-  };
-}
--- src/physics/pengine3d.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Physics Engine for 3d games
-
-namespace KissShock{
-  class PEngine3D{
-
-  };
-}
